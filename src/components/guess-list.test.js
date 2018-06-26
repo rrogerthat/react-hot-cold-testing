7b7361b1d9ab428ec8421c945751265a46a32de7
Roger Hwang <hroger19@gmail.com>
2018-06-26 10:42:26 -0700

    Remove yarn lock file

--- src/components/guess-list.test.js
+++ src/components/guess-list.test.js
@@ -8,14 +8,14 @@ describe('<GuessList />', () => {
 		shallow(<GuessList guesses={[]} />);	//so .map will be defined instead of undefined on something empty
 	});
 
-	// it('Renders a list of guesses', () => {
- //    	const values = [20, 40, 35];
- //    	const wrapper = shallow(<GuessList guesses={values} />);
- //    	const items = wrapper.find('li');
- //    	expect(items.length).toEqual(values.length);
- //    	values.forEach((value, index) => {
- //      	expect(items.at(index).text()).toEqual(value.toString());
- //    	});
- //    });	
+	it('Renders a list of guesses', () => {
+    	const values = [20, 40, 35];
+    	const wrapper = shallow(<GuessList guesses={values} />);
+    	const items = wrapper.find('li');
+    	expect(items.length).toEqual(values.length);
+    	values.forEach((value, index) => {
+      	expect(items.at(index).text()).toEqual(value.toString());
+    	});
+    });	
 
 });
